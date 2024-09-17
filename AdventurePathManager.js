c56321f3f9f9de665adc5f8cf084bbda5ad73a48
Maurus <147621326+MaurusPerry@users.noreply.github.com>
2024-09-17 14:25:14 -0300

    Solucionamos el Error y Avanazamos

--- AdventurePathManager.js
+++ AdventurePathManager.js
@@ -1,4 +1,19 @@
+//Pagina Principal
 function irPaginaPrincipal() {
-    console.log("Redirigiendo a PaginaPrincipal.html...");
     window.location.href = "Path0/PaginaPrincipal.html";
+}
+
+//Posponer la Rebelion
+function irPath1PosponeRebellion() {
+    window.location.href = "../Path0/Path1/Path1PosponeRebellion.html";
+}
+
+//Rebellion llevada acabo por Snowball
+function irPath1RebellionSnowball() {
+    window.location.href = "../Path0/Path1/SnowBall/Path1RebellionSnowball.html";
+}
+
+//Historia Original
+function irPath1HisotriaOriginal() {
+    window.location.href = "../Path0/Path1/OriginalHistory/Path1HisotriaOriginal.html";
 }
